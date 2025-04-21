2e3495ca6be469b15cc9bccae18402891e9aa5e4
Pronoy Malick <malickpronoy@gmail.com>
2025-04-21 22:53:33 +0530

    Improved the frontend and backend by adding image vizualization for the forecasted days

--- frontend.py
+++ frontend.py
@@ -2,12 +2,12 @@ import streamlit as st
 import plotly.express as px
 from backend import get_data
 
-st.set_page_config(layout="centered")
+st.set_page_config(layout = "centered")
 st.title("Weather Forecast for the next 5 days")
 place = st.text_input("Place", placeholder="Enter the location")
-days = st.slider("Number of Days", min_value=1, max_value=5)
+days = st.slider("Number of Days", min_value=1, max_value=5, help="Enter the Number of Forecast Days")
 option = st.selectbox("Choose Data to view", ("Temperature","Sky"))
-st.subheader(f"{option} for the next {days} in {place}")
+st.subheader(f"{option} for the next {days} days in {place}")
 
 if place:
     filtered_data =  get_data(place,days,option)
@@ -18,3 +18,9 @@ if place:
         st.plotly_chart(figure)
 
     if option == "Sky":
+        images = {"Clear": "Images/clear.png","Clouds": "Images/cloud.png",
+                  "Rain": "Images/rain.png","Snow": "Images/snow.png"}
+        sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+        image_paths = [images[condition] for condition in sky_conditions]
+        st.image(image_paths,width=120,caption=sky_conditions)
+
